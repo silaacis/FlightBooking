7a407215974823af31276530e708566f1abd8f1c
silaacis <161122870+silaacis@users.noreply.github.com>
2026-04-28 21:21:58 +0300

    #

---
+++ FlightBooking/Areas/Admin/Controllers/FlightsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class FlightsController : Controller
+    {
+        public IActionResult FlightList()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateFlight()
+        {
+            return View();
+        }
+    }
+}
---
+++ FlightBooking/Entities/Flight.cs
@@ -0,0 +1,26 @@
+﻿namespace FlightBooking.Entities
+{
+    public class Flight
+    {
+        public string FlightId { get; set; }
+        public string FlightNumber { get; set; } //TK1234, PC2023
+        public string AirlineCode { get; set; } //TK, PC, LH
+
+        public string DepartureAirportCode { get; set; } //IST, JFK, LHR
+        public string DepartureAirportName { get; set; } // Istanbul Airport, John F. Kennedy International Airport
+        public string ArrivalAirportCode { get; set; } //LHR, CDG, FRA
+        public string ArrivalAirportName { get; set; } //London Heathrow Airport, Charles de Gaulle Airport
+
+        public DateTime DepartureTime { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public int DurationMinutes { get; set; }
+
+        public int TotalSeats { get; set; }
+        public int AvailableSeats { get; set; }
+
+        public decimal BasePrice { get; set; }
+        public string Currency { get; set; } //USD, EUR, TRY
+        public string Status { get; set; } //Scheduled, Delayed, Cancelled, Boarding, Departed
+
+    }
+}
